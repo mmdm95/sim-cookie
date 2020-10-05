9ade04bf2502345ca7506cd0affdeabf991efb55
MMDM <mmdehghan95@gmail.com>
2020-10-06 02:02:10 +0330

    simple autoloader tested

--- src/Tests/index.php
+++ src/Tests/index.php
@@ -5,6 +5,7 @@ use Sim\Cookie\SetCookie;
 use Sim\Crypt\Crypt;
 
 include_once '../../vendor/autoload.php';
+//include_once '../../autoloader.php';
 
 $main_key = 'fDhIL1dmU2swMyl+VEUxR3gkJWRJO0RQNUxRUks2aFZZKDJsOVhVYzdCNE52eiEreU9fPkA=';
 $assured_key = 'eCtYfHRDOFVsOSV6aTZBNyk6Lyg+MGc0MTI8NTNKTXk=';

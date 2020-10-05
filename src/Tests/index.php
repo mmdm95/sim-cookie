09182237ec32f8c4cf43a4ebaa41f025cb24655b
MMDM <mmdehghan95@gmail.com>
2020-10-06 02:17:09 +0330

    uncomment accidentally commented line

--- src/Tests/index.php
+++ src/Tests/index.php
@@ -4,7 +4,7 @@ use Sim\Cookie\Cookie;
 use Sim\Cookie\SetCookie;
 use Sim\Crypt\Crypt;
 
-//include_once '../../vendor/autoload.php';
+include_once '../../vendor/autoload.php';
 
 //include_once '../../vendor/mmdm/sim-crypt/autoloader.php';
 //include_once '../../autoloader.php';
